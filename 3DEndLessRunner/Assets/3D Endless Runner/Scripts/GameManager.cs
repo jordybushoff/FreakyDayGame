4017d62749c288bcb400298d6f00fe2aa4137d62
mar121212 <okedanoke12345@gmail.com>
2019-05-07 13:03:51 +0200

    ssssss

--- 3DEndLessRunner/Assets/3D Endless Runner/Scripts/GameManager.cs
+++ 3DEndLessRunner/Assets/3D Endless Runner/Scripts/GameManager.cs
@@ -12,24 +12,19 @@ public enum GameState
     PouseGame,
     Continiue
 }
-
 public class GameManager : MonoBehaviour
 {
     public GameState CurrentState = GameState.Start;
 
-    public int Score = 0;
-    public int Loss = 0;
+    int Score;
     Player _Player;
     public Text TxtScore, TxtScoreGameOver, TxtBestScore, TxtCounter;
     public GameObject UiInGame;
     public GameObject BtnStart;
     public Animator _AnimCanvas;
 
-
     void Start()
     {
-        Score = PlayerPrefs.GetInt("Player Score");
-        Loss = PlayerPrefs.GetInt("Player Loss");
         _Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
         Refresh();
     }
@@ -71,10 +66,9 @@ public class GameManager : MonoBehaviour
                         PlayerPrefs.SetInt("BestScore", Score);
                     }
 
-                    PlayerPrefs.SetInt("Player Score", Score);
-                    PlayerPrefs.SetInt("Player Loss", Loss);
+
                     UiInGame.SetActive(false);
-                    TxtScoreGameOver.text = "Win: " + Score + " \n" + "Loss: " + Loss ;
+                    TxtScoreGameOver.text = "" + Score;
                     TxtBestScore.text = "" + PlayerPrefs.GetInt("BestScore");
                     _AnimCanvas.SetBool("EndGame", true);
 
@@ -121,6 +115,7 @@ public class GameManager : MonoBehaviour
     public void Btn_Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        //GetCurrentState (GameState.Ready);
     }
 
     public void Btn_Continiue()
@@ -128,117 +123,9 @@ public class GameManager : MonoBehaviour
         StartCoroutine("ContinueGame");
     }
 
-    public void GetScore(int NewScore, int NewLoss)
+    public void GetScore(int NewScore)
     {
         Score += NewScore;
-        Loss += NewLoss;
-        TxtScore.text = "Win: " + Score + "  Loss: " + Loss;
-
+        TxtScore.text = "" + Score;
     }
-
-public class GameManager : MonoBehaviour {
-	public GameState CurrentState = GameState.Start;
-
-	int Score;
-	Player _Player;
-	public Text TxtScore, TxtScoreGameOver, TxtBestScore,TxtCounter;
-	public GameObject UiInGame;
-	public GameObject BtnStart;
-	public Animator _AnimCanvas;
-
-	void Start(){
-		_Player = GameObject.FindGameObjectWithTag ("Player").GetComponent <Player> ();
-		Refresh ();
-	}
-
-
-	//Get Current State Game
-	public void GetCurrentState (GameState State){
-		CurrentState = State;
-		Refresh ();
-	}
-
-
-	public void Refresh(){
-		switch (CurrentState) {
-
-		case GameState.InGame:
-			{
-				Time.timeScale = 1;
-				UiInGame.SetActive (true);
-
-			}
-			break;
-
-		case GameState.Ready:
-			{
-
-				StartCoroutine ("GoToInGame");
-
-			}
-			break;
-		case GameState.EndGame:
-			{
-
-				if (Score > PlayerPrefs.GetInt ("BestScore")) {
-					PlayerPrefs.SetInt ("BestScore", Score);
-				}
-
-
-				UiInGame.SetActive (false);
-				TxtScoreGameOver.text = "" + Score;
-				TxtBestScore.text = "" + PlayerPrefs.GetInt ("BestScore");
-				_AnimCanvas.SetBool ("EndGame",true);
-
-			}
-			break;
-
-
-
-		}
-	}
-
-	public IEnumerator GoToInGame(){
-		yield return new WaitForSeconds (1f);
-		GetCurrentState (GameState.InGame);
-
-
-	}
-
-	public IEnumerator ContinueGame(){
-
-		_AnimCanvas.SetBool ("EndGame",false);
-		_Player.GetCurrentPlayerState (PlayerStates.Ready);
-		yield return new WaitForSeconds (1f);
-		TxtCounter.text = "" + 3;
-		yield return new WaitForSeconds (1f);
-		TxtCounter.text = "" + 2;
-		yield return new WaitForSeconds (1f);
-		TxtCounter.text = "" + 1;
-		yield return new WaitForSeconds (1f);
-		TxtCounter.text = "";
-
-		GetCurrentState (GameState.InGame);
-
-	}
-
-	public void StartGame(){
-		GetCurrentState (GameState.Ready);
-		BtnStart.SetActive (false);
-	}
-
-	public void Btn_Restart(){
-		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
-		//GetCurrentState (GameState.Ready);
-	}
-
-	public void Btn_Continiue(){
-		StartCoroutine ("ContinueGame");
-	}
-
-	public void GetScore(int NewScore){
-		Score += NewScore;
-		TxtScore.text = "" + Score;
-	}
-
 }
--- 3DEndLessRunner/Assets/3D Endless Runner/Scripts/Player.cs
+++ 3DEndLessRunner/Assets/3D Endless Runner/Scripts/Player.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Linq;
+
 
 public enum PlayerStates
 {
@@ -9,7 +9,6 @@ public enum PlayerStates
     Live,
     Die
 }
-
 public class Player : MonoBehaviour
 {
     public PlayerStates CurrentPlayerState = PlayerStates.Start;
@@ -17,56 +16,36 @@ public class Player : MonoBehaviour
     GameManager _GameManager;
     Animator _Animator;
     Vector3 LastPosition;
+
     GameObject player;
+    GameObject trapup;
+    GameObject trapdown;
+
+
     AudioSource _AudioSource;
     public int JumpForce;
+    public int ZweefForce;
     public int Speed;
-    public float PilarDistancex;
+
+    public float PilarDistanceup;
+    public float PilarDistancedown;
+    //public float PilarDistancedown;
     public AudioClip JetPackAudio;
+
+
+
     public AudioClip DieAudio;
     public ParticleSystem SmokeParticle;
-    public bool isDead = false;
-
 
     void Awake()
     {
 
-public class Player : MonoBehaviour {
-	public PlayerStates CurrentPlayerState = PlayerStates.Start;
-	Rigidbody _Rigidbody;
-	GameManager _GameManager;
-	Animator _Animator;
-	Vector3 LastPosition;
-
-    GameObject player;
-    GameObject trapup;
-	GameObject trapdown;
-
-
-	AudioSource _AudioSource;
-	public int JumpForce;
-	public int ZweefForce;
-	public int Speed;
-
-    public float PilarDistanceup;
-    public float PilarDistancedown;
-	//public float PilarDistancedown;
-    public AudioClip JetPackAudio;
-
-
-
-	public AudioClip DieAudio;
-	public ParticleSystem SmokeParticle;
-
-
 
         //  I have used -200 gravity in this game
-        Physics.gravity = new Vector3(0, -350f, 0);
+        Physics.gravity = new Vector3(0, -200f, 0);  //-200f bepaald valsnelheid wanneer er niet wordt geklikt. Lijkt geen effect te hebben op afstanden en snelheid.
 
     }
 
-		//  I have used -200 gravity in this game
-		Physics.gravity = new Vector3(0, -200f,0);  //-200f bepaald valsnelheid wanneer er niet wordt geklikt. Lijkt geen effect te hebben op afstanden en snelheid.
 
     void Start()
     {
@@ -74,213 +53,169 @@ public class Player : MonoBehaviour {
         _Animator = GetComponent<Animator>();
         _AudioSource = GetComponent<AudioSource>();
         _GameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        trapup = GameObject.FindGameObjectWithTag("TrapUp");
+        trapdown = GameObject.FindGameObjectWithTag("Trap");
+        //trapdown = GameObject.FindGameObjectWithTag("Score");
     }
+
+
+
 
 
     void Update()
     {
         if (_GameManager.CurrentState == GameState.InGame)
-        {
-            _AudioSource.clip = JetPackAudio;
-            if (_AudioSource.isPlaying == false)
-                _AudioSource.Play();
-            _Animator.SetBool("InGame", true);
-            GetCurrentPlayerState(PlayerStates.Live);
+        { //Zodra dit geldig is, voer het volgende uit:
+            _AudioSource.clip = JetPackAudio; //Sound effect wanneer er gesprongen wordt
+            if (_AudioSource.isPlaying == false)//Als het geluid niet afspeelt --->
+                _AudioSource.Play();// ---> Speel het geluid af.
+            _Animator.SetBool("InGame", true); //geeft een boolean value door aan een animatie controller
+            GetCurrentPlayerState(PlayerStates.Live); // De game is in de live state.
         }
+
 
         if (CurrentPlayerState == PlayerStates.Live)
         {
-            var mlGoalObjects = GameObject.FindGameObjectsWithTag("MLGoalTag");
+
             player = GameObject.FindGameObjectWithTag("Player");
             _Rigidbody.velocity = new Vector2(Speed, _Rigidbody.velocity.y); // Speed
 
-            RaycastHit Geraakt;
-
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Geraakt))
-            {
-                PilarDistancex = Geraakt.distance;
-
-
-	void Start () {
-        _Rigidbody = GetComponent <Rigidbody> ();
-		_Animator  = GetComponent <Animator> ();
-		_AudioSource = GetComponent <AudioSource> ();
-		_GameManager = GameObject.FindGameObjectWithTag ("GameManager").GetComponent <GameManager> ();
-        trapup = GameObject.FindGameObjectWithTag("TrapUp");
-        trapdown = GameObject.FindGameObjectWithTag("Trap");
-		//trapdown = GameObject.FindGameObjectWithTag("Score");
-    }
-
-
-
-
-
-	void Update () {
-		if (_GameManager.CurrentState == GameState.InGame) { //Zodra dit geldig is, voer het volgende uit:
-			_AudioSource.clip = JetPackAudio; //Sound effect wanneer er gesprongen wordt
-			if (_AudioSource.isPlaying == false)//Als het geluid niet afspeelt --->
-				_AudioSource.Play ();// ---> Speel het geluid af.
-			_Animator.SetBool ("InGame", true); //geeft een boolean value door aan een animatie controller
-			GetCurrentPlayerState (PlayerStates.Live); // De game is in de live state.
-		}
-
-
-		if (CurrentPlayerState == PlayerStates.Live) {
-
-			player = GameObject.FindGameObjectWithTag ("Player");
-			_Rigidbody.velocity = new Vector2 (Speed, _Rigidbody.velocity.y); // Speed
-
-
-			RaycastHit Geraakt1;
-			//RaycastHit Geraakt2;
-			//RaycastHit Geraakt2;
-
-			if (Physics.Raycast (transform.position, transform.TransformDirection (Vector3.forward), out Geraakt1)) {
-				PilarDistanceup = Geraakt1.distance;
-				//PilarDistanceup = trapup.gameObject.transform.position.x;
-				//PilarDistancedown = Geraakt2.distance;
-				//PilarDistancedown = Geraakt2.distance;
-
-
-           // RaycastHit Geraakt1;
+            RaycastHit Geraakt1;
+            //RaycastHit Geraakt2;
             //RaycastHit Geraakt2;
 
-            //if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Geraakt1))
-            //{
-            //    PilarDistanceup = Geraakt1.distance;
-
-            //    if (PilarDistanceup < 100f)
-            //    {
-            //       _Rigidbody.velocity = new Vector2(_Rigidbody.velocity.x, JumpForce);
-            //   }
-            //    else if (PilarDistanceup < 50f)
-            //    {
-
-            //   }
-            // }
-
-				if (PilarDistanceup < 60f) {
-					_Rigidbody.velocity = new Vector2 (_Rigidbody.velocity.x, JumpForce);
-				}
-				//else if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Geraakt2))
-               // {
-
-
-
-
-            if (Input.GetMouseButton(0))  // if left mouse button pressed
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Geraakt1))
             {
+                PilarDistanceup = Geraakt1.distance;
+                //PilarDistanceup = trapup.gameObject.transform.position.x;
+                //PilarDistancedown = Geraakt2.distance;
+                //PilarDistancedown = Geraakt2.distance;
 
-                _Rigidbody.velocity = new Vector2(_Rigidbody.velocity.x, JumpForce); // Jump
+
+                // RaycastHit Geraakt1;
+                //RaycastHit Geraakt2;
+
+                //if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Geraakt1))
+                //{
+                //    PilarDistanceup = Geraakt1.distance;
+
+                //    if (PilarDistanceup < 100f)
+                //    {
+                //       _Rigidbody.velocity = new Vector2(_Rigidbody.velocity.x, JumpForce);
+                //   }
+                //    else if (PilarDistanceup < 50f)
+                //    {
+
+                //   }
+                // }
+
+                if (PilarDistanceup < 60f)
+                {
+                    _Rigidbody.velocity = new Vector2(_Rigidbody.velocity.x, JumpForce);
+                }
+                //else if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Geraakt2))
+                // {
+
+                // }
+
+                else if (PilarDistanceup > 30f)
+                {
+                    //_Rigidbody.velocity = new Vector2(_Rigidbody.velocity.x, ZweefForce);
+                }
+
+
             }
-            /*
-
-              // }
-
-				else if (PilarDistanceup > 30f) {
-					//_Rigidbody.velocity = new Vector2(_Rigidbody.velocity.x, ZweefForce);
-				}
-
-
-			}
 
 
 
 
-			// if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Geraakt2, 1 << 10))
-			// {
-			//     PilarDistancedown = Geraakt2.distance;
+            // if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Geraakt2, 1 << 10))
+            // {
+            //     PilarDistancedown = Geraakt2.distance;
 
-			//     if (PilarDistancedown < 100f)
-			//     {
+            //     if (PilarDistancedown < 100f)
+            //     {
 
 
             //    }
-           // }
+            // }
 
             //if (player.transform.position.y < -30f || player.transform.position.y < -30f && PilarDistanceup == 0 || player.transform.position.y < -30f && PilarDistancedown == 0)
             //{
             //    _Rigidbody.velocity = new Vector2(_Rigidbody.velocity.x, JumpForce);
             //}
-            if(PilarDistanceup >= player.transform.position.y)
+            if (PilarDistanceup >= player.transform.position.y)
             {
-                if(PilarDistancedown >= player.transform.position.y)
+                if (PilarDistancedown >= player.transform.position.y)
                 {
-                _Rigidbody.velocity = new Vector2(_Rigidbody.velocity.x, JumpForce);
+                    _Rigidbody.velocity = new Vector2(_Rigidbody.velocity.x, JumpForce);
                 }
             }
 
             //if (Input.GetMouseButton (0))  // if left mouse button pressed
-			//{
+            //{
 
-			//    }
-			// }
+            //    }
+            // }
 
-			if (player.transform.position.y < -80f || player.transform.position.y < -80f && PilarDistancedown == 0 || player.transform.position.y < -80f && PilarDistancedown == 0) {
-				_Rigidbody.velocity = new Vector2 (_Rigidbody.velocity.x, JumpForce);
-			}
+            if (player.transform.position.y < -80f || player.transform.position.y < -80f && PilarDistancedown == 0 || player.transform.position.y < -80f && PilarDistancedown == 0)
+            {
+                _Rigidbody.velocity = new Vector2(_Rigidbody.velocity.x, JumpForce);
+            }
 
-			if (CurrentPlayerState == PlayerStates.Live) { //wanneer de huidige player state live is, voer het volgende uit:
+            if (CurrentPlayerState == PlayerStates.Live)
+            { //wanneer de huidige player state live is, voer het volgende uit:
 
-				_Rigidbody.velocity = new Vector2 (Speed, _Rigidbody.velocity.y); //Snelheid van de physics (verticale jump physics?)
+                _Rigidbody.velocity = new Vector2(Speed, _Rigidbody.velocity.y); //Snelheid van de physics (verticale jump physics?)
 
-				if (Input.GetMouseButton (0)) {  // Wanneer de linkermuis wordt geklikt --->
-
-
-					_Rigidbody.velocity = new Vector2 (_Rigidbody.velocity.x, JumpForce); // ----> Spring
-				}
-
+                if (Input.GetMouseButton(0))
+                {  // Wanneer de linkermuis wordt geklikt --->
 
 
-				if (Input.GetMouseButtonDown (0)) {
-					SmokeParticle.Play ();
-				} else if (Input.GetMouseButtonUp (0)) {
-					SmokeParticle.Stop ();
-				}
+                    _Rigidbody.velocity = new Vector2(_Rigidbody.velocity.x, JumpForce); // ----> Spring
+                }
 
-				*/
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    SmokeParticle.Play();
+                }
+                else if (Input.GetMouseButtonUp(0))
+                {
+                    SmokeParticle.Stop();
+                }
+
+
+            }
         }
-        if (player.transform.position.y < -30f && PilarDistancex > 150f || player.transform.position.y < -30f && PilarDistancex == 0)
-        {
-            Flap();
-        }
-
-    }
-    public void Flap()
-    {
-        _Rigidbody.velocity = new Vector2(_Rigidbody.velocity.x, JumpForce); // Jump
     }
 
 
-    void OnCollisionEnter(Collision Coll)
+    void OnCollisionEnter(Collision Coll) //Wanneer er een collision met een trap plaatsvindt:
     {
         if (CurrentPlayerState == PlayerStates.Live)
         {
-
-            if (Coll.gameObject.tag == "Trap")
+            if (Coll.gameObject.tag == "Trap") //W
             {
-                _GameManager.GetScore(0, 1);
-                LastPosition = new Vector3(transform.position.x + 3, 0, 0);
-                GetCurrentPlayerState(PlayerStates.Die);
-                isDead = true;
-
+                LastPosition = new Vector3(transform.position.x + 3, 0, 0); // reset player naar de volgende positie
+                GetCurrentPlayerState(PlayerStates.Die); //Verander player state naar, die
             }
         }
 
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other) //wanneer de speler succesvol door een trap springt (???)
     {
         if (CurrentPlayerState == PlayerStates.Live)
         {
-            if (other.tag == "MLGoalTag")
+            if (other.tag == "Score")
             {
-               _GameManager.GetScore(1, 0);
+                _GameManager.GetScore(1); //Tel 1 puntje er bij
             }
         }
 
     }
-
+    //
     public void GetCurrentPlayerState(PlayerStates State)
     {
         CurrentPlayerState = State; //GetState
@@ -291,97 +226,29 @@ public class Player : MonoBehaviour {
     {
         switch (CurrentPlayerState)
         {
-            case PlayerStates.Ready:
+            case PlayerStates.Ready: //Wanneer de state, ready is
                 {
-                    transform.position = LastPosition;
-                    transform.rotation = Quaternion.Euler(0, 0, 0);
-                    _Rigidbody.velocity = new Vector3(0, 0, 0);
-                    _Rigidbody.useGravity = false;
-                    _Rigidbody.freezeRotation = true;
+                    transform.position = LastPosition; //(Default positie?)
+                    transform.rotation = Quaternion.Euler(0, 0, 0); // returned een rotatie die Z graden roteerd om de Z As, X graden om de X as en y graden om de y as.
+                    _Rigidbody.velocity = new Vector3(0, 0, 0); // Velocity geven aan de de rigidbody
+                    _Rigidbody.useGravity = false; // bepaalt of gravity de rigidbody affects, wanneer op false, gedraagt de rigidbody alsof hij in de ruimte zweeft
+                    _Rigidbody.freezeRotation = true; // bepaalt of physics de rotatie van het object beinvloeden
                 }
                 break;
-            case PlayerStates.Live:
+            case PlayerStates.Live: // als state live is
                 {
-                    _Rigidbody.useGravity = true;
+                    _Rigidbody.useGravity = true; // zorgt ervoor dat gravity actief wordt en de rigidbody daarmee beinvloed.
                 }
                 break;
-            case PlayerStates.Die:
+            case PlayerStates.Die: // als state die is
                 {
-
-                    _Rigidbody.freezeRotation = false;
-                    _AudioSource.PlayOneShot(DieAudio);
-                    _Rigidbody.velocity = new Vector3(0, 0, 0);
-
-                    _GameManager.GetCurrentState(GameState.EndGame);
+                    _Rigidbody.freezeRotation = false; //physics beinvloeden de rotatie van het object niet meer
+                    _AudioSource.PlayOneShot(DieAudio); //speel audio effect af wanneer de state die is.
+                    _Rigidbody.velocity = new Vector3(0, 0, 0); //Velocity geven aan de rigidbody (Andere waarden dan de standaardwaarden kunnen onverwachtse effecten geven)
+                    _GameManager.GetCurrentState(GameState.EndGame); //Ga naar endgame state.
                 }
                 break;
         }
-    }
-
-
-
-
-			}
-		}
-	}
-
-
-	void OnCollisionEnter(Collision Coll) //Wanneer er een collision met een trap plaatsvindt:
-	{
-		if (CurrentPlayerState == PlayerStates.Live) {
-			if (Coll.gameObject.tag == "Trap") //W
-			{
-				LastPosition = new Vector3 (transform.position.x+3,0,0); // reset player naar de volgende positie
-				GetCurrentPlayerState (PlayerStates.Die); //Verander player state naar, die
-			}
-		}
-
-	}
-
-	void OnTriggerEnter(Collider other) //wanneer de speler succesvol door een trap springt (???)
-	{
-		if (CurrentPlayerState == PlayerStates.Live) {
-			if (other.tag == "Score")
-			{
-				_GameManager.GetScore (1); //Tel 1 puntje er bij
-			}
-		}
-
-	}
-	//
-	public void GetCurrentPlayerState (PlayerStates State)
-	{
-		CurrentPlayerState = State; //GetState
-		Refresh ();
-	}
-
-	void Refresh(){
-		switch (CurrentPlayerState)
-		{
-		case PlayerStates.Ready: //Wanneer de state, ready is
-			{
-				transform.position = LastPosition; //(Default positie?)
-				transform.rotation = Quaternion.Euler (0,0,0); // returned een rotatie die Z graden roteerd om de Z As, X graden om de X as en y graden om de y as.
-				_Rigidbody .velocity = new Vector3 (0,0,0); // Velocity geven aan de de rigidbody
-				_Rigidbody.useGravity = false; // bepaalt of gravity de rigidbody affects, wanneer op false, gedraagt de rigidbody alsof hij in de ruimte zweeft
-				_Rigidbody.freezeRotation = true; // bepaalt of physics de rotatie van het object beinvloeden
-			}
-			break;
-		case PlayerStates.Live: // als state live is
-			{
-				_Rigidbody.useGravity = true; // zorgt ervoor dat gravity actief wordt en de rigidbody daarmee beinvloed.
-			}
-			break;
-		case PlayerStates.Die: // als state die is
-			{
-				_Rigidbody.freezeRotation = false; //physics beinvloeden de rotatie van het object niet meer
-				_AudioSource.PlayOneShot (DieAudio); //speel audio effect af wanneer de state die is.
-				_Rigidbody.velocity = new Vector3 (0,0,0); //Velocity geven aan de rigidbody (Andere waarden dan de standaardwaarden kunnen onverwachtse effecten geven)
-				_GameManager.GetCurrentState (GameState.EndGame); //Ga naar endgame state.
-			}
-			break;
-		}
         //
-	}
-
+    }
 }
