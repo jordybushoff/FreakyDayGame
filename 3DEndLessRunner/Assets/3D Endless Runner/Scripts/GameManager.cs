ae9b77664b8bcb41cd69ff3103374ffd852699ed
jordybushoff <47558264+jordybushoff@users.noreply.github.com>
2019-05-23 13:07:22 +0200

    gh

--- 3DEndLessRunner/Assets/3D Endless Runner/Scripts/GameManager.cs
+++ 3DEndLessRunner/Assets/3D Endless Runner/Scripts/GameManager.cs
@@ -1,131 +1,117 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-public enum GameState
-{
-
-    Start,
-    Ready,
-    InGame,
-    EndGame,
-    PouseGame,
-    Continiue
+using UnityEngine .SceneManagement;
+using UnityEngine .UI;
+public enum GameState{
+	
+	Start,
+	Ready,
+	InGame,
+	EndGame,
+	PouseGame,
+	Continiue
 }
-public class GameManager : MonoBehaviour
-{
-    public GameState CurrentState = GameState.Start;
+public class GameManager : MonoBehaviour {
+	public GameState CurrentState = GameState.Start;
+	 
+	int Score;
+	Player _Player;
+	public Text TxtScore, TxtScoreGameOver, TxtBestScore,TxtCounter;
+	public GameObject UiInGame;
+	public GameObject BtnStart;
+	public Animator _AnimCanvas;
 
-    int Score;
-    Player _Player;
-    public Text TxtScore, TxtScoreGameOver, TxtBestScore, TxtCounter;
-    public GameObject UiInGame;
-    public GameObject BtnStart;
-    public Animator _AnimCanvas;
-
-    void Start()
-    {
-        _Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
-        Refresh();
-    }
+	void Start(){
+		_Player = GameObject.FindGameObjectWithTag ("Player").GetComponent <Player> ();
+		Refresh (); 
+	}
 
 
-    //Get Current State Game
-    public void GetCurrentState(GameState State)
-    {
-        CurrentState = State;
-        Refresh();
-    }
+	//Get Current State Game
+	public void GetCurrentState (GameState State){
+		CurrentState = State;
+		Refresh ();
+	}
 
 
-    public void Refresh()
-    {
-        switch (CurrentState)
-        {
+	public void Refresh(){
+		switch (CurrentState) {
+		 
+		case GameState.InGame:
+			{
+				Time.timeScale = 1;
+				UiInGame.SetActive (true);
 
-            case GameState.InGame:
-                {
-                    Time.timeScale = 1;
-                    UiInGame.SetActive(true);
+			}
+			break;
 
-                }
-                break;
+		case GameState.Ready:
+			{
+				
+				StartCoroutine ("GoToInGame");
+			 
+			}
+			break;
+		case GameState.EndGame:
+			{
 
-            case GameState.Ready:
-                {
-
-                    StartCoroutine("GoToInGame");
-
-                }
-                break;
-            case GameState.EndGame:
-                {
-
-                    if (Score > PlayerPrefs.GetInt("BestScore"))
-                    {
-                        PlayerPrefs.SetInt("BestScore", Score);
-                    }
+				if (Score > PlayerPrefs.GetInt ("BestScore")) {
+					PlayerPrefs.SetInt ("BestScore", Score);
+				}
 
 
-                    UiInGame.SetActive(false);
-                    TxtScoreGameOver.text = "" + Score;
-                    TxtBestScore.text = "" + PlayerPrefs.GetInt("BestScore");
-                    _AnimCanvas.SetBool("EndGame", true);
+				UiInGame.SetActive (false);
+				TxtScoreGameOver.text = "" + Score;
+				TxtBestScore.text = "" + PlayerPrefs.GetInt ("BestScore");
+				_AnimCanvas.SetBool ("EndGame",true);
+				 
+			}
+			break;
 
-                }
-                break;
+		
+
+		}
+	}
+
+	public IEnumerator GoToInGame(){
+		yield return new WaitForSeconds (1f);
+		GetCurrentState (GameState.InGame);
 
 
+	}
 
-        }
-    }
+	public IEnumerator ContinueGame(){
+		 
+		_AnimCanvas.SetBool ("EndGame",false);
+		_Player.GetCurrentPlayerState (PlayerStates.Ready);
+		yield return new WaitForSeconds (1f);
+		TxtCounter.text = "" + 3;
+		yield return new WaitForSeconds (1f);
+		TxtCounter.text = "" + 2;
+		yield return new WaitForSeconds (1f);
+		TxtCounter.text = "" + 1;
+		yield return new WaitForSeconds (1f);
+		TxtCounter.text = "";
 
-    public IEnumerator GoToInGame()
-    {
-        yield return new WaitForSeconds(1f);
-        GetCurrentState(GameState.InGame);
+		GetCurrentState (GameState.InGame);
 
+	}
 
-    }
+	public void StartGame(){
+		GetCurrentState (GameState.Ready);
+		BtnStart.SetActive (false);
+	}
 
-    public IEnumerator ContinueGame()
-    {
+	public void Btn_Restart(){
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+	}
 
-        _AnimCanvas.SetBool("EndGame", false);
-        _Player.GetCurrentPlayerState(PlayerStates.Ready);
-        yield return new WaitForSeconds(1f);
-        TxtCounter.text = "" + 3;
-        yield return new WaitForSeconds(1f);
-        TxtCounter.text = "" + 2;
-        yield return new WaitForSeconds(1f);
-        TxtCounter.text = "" + 1;
-        yield return new WaitForSeconds(1f);
-        TxtCounter.text = "";
+	public void Btn_Continiue(){
+		StartCoroutine ("ContinueGame");
+	}
 
-        GetCurrentState(GameState.InGame);
-
-    }
-
-    public void StartGame()
-    {
-        GetCurrentState(GameState.Ready);
-        BtnStart.SetActive(false);
-    }
-
-    public void Btn_Restart()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        //GetCurrentState (GameState.Ready);
-    }
-
-    public void Btn_Continiue()
-    {
-        StartCoroutine("ContinueGame");
-    }
-
-    public void GetScore(int NewScore)
-    {
-        Score += NewScore;
-        TxtScore.text = "" + Score;
-    }
+	public void GetScore(int NewScore){
+		Score += NewScore;
+		TxtScore.text = "" + Score;
+	}
 }
